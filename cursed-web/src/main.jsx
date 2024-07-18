a3a368f72e14bd7facee0524d1527bb708f442e9
Alex Carter (victoryforphil) <alex.carter@flyzipline.com>
2024-07-18 11:58:07 -0700

    Removed dockview CSS

--- cursed-web/src/main.jsx
+++ cursed-web/src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import 'dockview/dist/styles/dockview.css';
-
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
